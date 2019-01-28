7f3e3408d38ba8c12c437162ca996d024b4a7737
이교택 <leegyotaek@gmail.com>
2019-01-28 15:32:48 +0900

    this is second commit for test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
+            Learn React test
           </a>
         </header>
       </div>
